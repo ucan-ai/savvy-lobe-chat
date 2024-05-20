632bb089f327341bd574ccf3f62c11485b425e95
dpranav <kumarpranav@live.com>
2024-05-19 22:12:35 -0400

    name change

--- src/app/(main)/welcome/_layout/Desktop.tsx
+++ src/app/(main)/welcome/_layout/Desktop.tsx
@@ -4,7 +4,7 @@ import { Flexbox } from 'react-layout-kit';
 
 import Follow from '@/features/Follow';
 
-const COPYRIGHT = `© ${new Date().getFullYear()} LobeHub, LLC`;
+const COPYRIGHT = `© ${new Date().getFullYear()} UCAN AI, LLC`;
 
 const DesktopLayout = ({ children }: PropsWithChildren) => {
   return (
