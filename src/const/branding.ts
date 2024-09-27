42bc4c5b832a0ac26666d3e2114ee5e697b5d714
Pranav <kumarpranav@live.com>
2024-09-26 23:18:48 -0400

    changed branding

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,9 +2,9 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'UCAN AI Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
+export const BRANDING_NAME = 'Savvy AI';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'SavvyAI';
